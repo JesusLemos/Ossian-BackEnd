d10b95ec2e7e789ea1e1edc4e777cb04f8ecb2d2
JesusLemos <jesuslemosfoncubierta@gmail.com>
2020-06-15 11:47:37 +0200

    añadido controlado buscando usuario por id

--- routes/agenda.js
+++ routes/agenda.js
@@ -3,12 +3,13 @@ var router = express.Router();
 
 const addUser = require('../controller/addUser_controller');
 const getAll = require('../controller/getAllUser_controller');
+const findOneuser = require('../controller/findOneUser_controller');
 
 // router.get('/', function(req, res, next) {
 //     res.render('index', { title: 'Agenda' });
 //   });
 
-
+router.get('/findoneuser/:id', findOneuser);
 router.get('/getall', getAll);
 router.post('/adduser', addUser );
 
