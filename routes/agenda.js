46b43c6755562310d6f956776d93c9f66815634f
JesusLemos <jesuslemosfoncubierta@gmail.com>
2020-06-15 16:25:55 +0200

    Añadido el controlador updateUser

--- routes/agenda.js
+++ routes/agenda.js
@@ -5,6 +5,7 @@ const addUser = require('../controller/addUser_controller');
 const getAll = require('../controller/getAllUser_controller');
 const findOneuser = require('../controller/findOneUser_controller');
 const filterUser = require('../controller/filterUser_controller');
+const updateUser = require('../controller/updateUser_controller');
 const deleteUser_controller = require('../controller/deleteUser_controller');
 
 // router.get('/', function(req, res, next) {
@@ -16,6 +17,8 @@ router.get('/findoneuser/:id', findOneuser);
 router.get('/filteruser/:nombre', filterUser);
 router.post('/adduser', addUser );
 
+
+router.patch('/update/:id', updateUser)
 router.delete('/delete/:id', deleteUser_controller);
 
 module.exports = router;
